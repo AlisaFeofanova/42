6de04d13790a9491f2d4eeac0df66d5923259a86
Alisa Feofanova <32147814+AlisaFeofanova@users.noreply.github.com>
2025-10-15 17:48:30 +0200

    Update ft_isalnum.c
    libft part 1

---
+++ libft/ft_isalnum.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alfeofan <alfeofan@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 12:50:13 by alfeofan          #+#    #+#             */
+/*   Updated: 2025/09/29 13:56:20 by alfeofan         ###   ####lausanne.ch   */
+/*                                                                            */
+/* ************************************************************************** */
+int		ft_isalnum(char c)
+{
+	if((c >= 'A' && c <= 'Z')
+	 || c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+/*
+#include <stdio.h>
+
+int main ()
+{
+	int i;
+	char str[]= "Alisa+_)6433/&%$@(&#";
+	i=0;
+	while(str[i])
+	{
+		if (ft_isalnum(str[i])) printf ("character %c is alphanumeric\n",str[i]);
+     	else printf ("character %c is not alphanumeric\n",str[i]);
+     	i++;
+	}
+  return 0;
+}*/
