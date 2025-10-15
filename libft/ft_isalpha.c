a941afcdaa4a1bc3b439e076646a24e00b22d8f2
Alisa Feofanova <32147814+AlisaFeofanova@users.noreply.github.com>
2025-10-15 17:49:00 +0200

    Update ft_isalpha.c
    libft part 1

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -1,20 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alfeofan <alfeofan@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/29 12:50:13 by alfeofan          #+#    #+#             */
-/*   Updated: 2025/09/29 13:56:20 by alfeofan         ###   ####lausanne.ch   */
+/*   Created: 2025/09/29 12:34:49 by alfeofan          #+#    #+#             */
+/*   Updated: 2025/09/29 12:42:50 by alfeofan         ###   ####lausanne.ch   */
 /*                                                                            */
 /* ************************************************************************** */
-int		ft_isalnum(char c)
+int		ft_isalpha(char c)
 {
 	if((c >= 'A' && c <= 'Z')
 	 || c >= 'a' && c <= 'z')
-		return (1);
-	else if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
@@ -24,14 +22,13 @@ int		ft_isalnum(char c)
 
 int main ()
 {
-	int i;
-	char str[]= "Alisa+_)6433/&%$@(&#";
-	i=0;
-	while(str[i])
-	{
-		if (ft_isalnum(str[i])) printf ("character %c is alphanumeric\n",str[i]);
-     	else printf ("character %c is not alphanumeric\n",str[i]);
-     	i++;
-	}
+  int i=0;
+  char str[]="Alisa+_)6433/&%$@(&#";
+  while (str[i])
+  {
+    if (ft_isalpha(str[i])) printf ("character %c is alphabetic\n",str[i]);
+    else printf ("character %c is not alphabetic\n",str[i]);
+    i++;
+  }
   return 0;
 }*/
