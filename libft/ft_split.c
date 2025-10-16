d67df677011b696859f18a83872d5afc000fdea2
Alisa Feofanova <32147814+AlisaFeofanova@users.noreply.github.com>
2025-10-16 15:28:03 +0200

    Update ft_split.c
    split -25

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -91,3 +91,47 @@ char	**ft_split(char const *s, char c)
 	result[j] = NULL;
 	return (result);
 }
+
+
+
+
+
+
+
+ 61 int word_len(char const *str, char c)
+ 62 {
+ 63     int     len;
+ 64 
+ 65     len = 0;
+ 66     while (s[len] == c)
+ 67             len++;
+ 68     while (s[len] && s[len] != c)
+ 69             len++;
+ 70     return (len);
+ 71 }
+ 72 
+ 73 char    **ft_split(char const *s, char c)
+ 74 {
+ 75     char    **result;
+ 76     int     j;
+ 77     int end;
+ 78 
+ 79     if (!s || !c)
+ 80         return (NULL);
+ 81     result = malloc((word_count(s, c) + 1) * sizeof(char *));
+ 82     if (!result)
+ 83         return (NULL);
+ 84     j = 0;
+ 85     while (*s)
+ 86     {   
+ 87         end = word_len(s, c);
+ 88         result[j] = fill_word(s, 0, end);
+ 89         if (!(result[j]))
+ 90             return (ft_free(result, j));
+ 91         j++;
+ 92         s += end;
+ 93     }
+ 94     result[j] = NULL;
+ 95     return (result);
+ 96 }
+
