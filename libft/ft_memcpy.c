a19e178ad3bc7cdfdb230e5d120c4066247419e6
Alisa Feofanova <32147814+AlisaFeofanova@users.noreply.github.com>
2025-11-04 17:19:37 +0100

    Update ft_memcpy.c
    fin memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -22,7 +22,7 @@
 */
 #include "libft.h"
 
-void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t				i;
 	unsigned char		*d;
@@ -31,6 +31,8 @@ void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
 	i = 0;
+	if (!dest && !src)
+		return (NULL);
 	while (i < n)
 	{
 		d[i] = s[i];
