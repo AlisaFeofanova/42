337c54b15082f43ccab7749a538812ab1cb1346b
Alisa Feofanova <32147814+AlisaFeofanova@users.noreply.github.com>
2026-02-11 18:52:58 +0100

    Update ft_putptr.c
    v1 put ptr

--- ft_printf/ft_putptr.c
+++ ft_printf/ft_putptr.c
@@ -1,28 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putptr.c                                        :+:      :+:    :+:   */
+/*   ft_putptr.c                                         :+:    :+:           */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alfeofan <alfeofan@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/15 18:39:41 by alfeofan          #+#    #+#             */
-/*   Updated: 2025/10/16 14:59:40 by alfeofan         ###   ####lausanne.ch   */
+/*   Created: 2026/02/11 18:06:17 by alfeofan          #+#    #+#             */
+/*   Updated: 2026/02/11 18:45:13 by alfeofan       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "ft_printf.h"
+
+static int	hex(unsigned long n, int format)
+{
+	int			count;
+	const char	*base;
+
+	count = 0;
+	if (format == 0)
+		base = "0123456789abcdef";
+	else
+		base = "0123456789ABCDEF";
+	if (n >= 16)
+		count += hex(n / 16, format);
+	count += ft_putchar(base[n % 16]);
+	return (count);
+}
 
 int	ft_putptr(void *ptr)
 {
-	int			len;
-	long		address;
+	int				len;
+	unsigned long	address;
 
 	address = (unsigned long)ptr;
 	len = 0;
-	len += write(1, "0x", 2);
 	if (!address)
-		return (len + write(1, "0", 1));
-	return (len + ft_puthex(address, 0));
+		return (write(1, "(nil)", 5), 5);
+	len += write(1, "0x", 2);
+	return (len + hex(address, 0));
 }
+
 /*
 int main(void) {
     int *ptr;
